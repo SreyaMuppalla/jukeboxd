6d6fe69759741bcfa9cf37938de498ad66c2770c
Anthony Chen <anchen082016@gmail.com>
2025-02-11 19:11:09 -0800

    migrated feed page

--- jukeboxd-next/src/bigcomponents/SongsCarousal.js
+++ jukeboxd-next/src/bigcomponents/SongsCarousal.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Box, Typography, Rating } from "@mui/material"; // Import Material UI components
 import { CarouselContainer, SongItem, AlbumCover, SongName, StarsContainer } from "../styles/StyledComponents"; // Adjust based on your folder structure
 import albumpic from "../images/albumpic.jpg";
-import { useNavigate } from "react-router-dom"; // Import useNavigate for navigation
+import Link from "next/link";
+import Image from "next/image";
 
 const SongsCarousel = () => {
-  const navigate = useNavigate(); // Hook to handle navigation
 
   // Placeholder data for songs
   const songs = new Array(10).fill({ albumCover: albumpic, songName: "Song Name", stars: 5 });
@@ -16,22 +16,22 @@ const SongsCarousel = () => {
         <SongItem key={index}>
           {/* Album cover */}
           <AlbumCover
-            onClick={() => navigate("/album-page")} // Navigate to AlbumPage when clicked
-            style={{ cursor: "pointer" }} // Change cursor to indicate it's clickable
+            style={{ cursor: 'pointer' }} // Change cursor to indicate it's clickable
           >
-            <img
-              src={song.albumCover} // Use the imported album picture
-              alt={`Album cover for ${song.songName}`}
-              width="100%" // Ensures the image takes the full width of the container
-            />
+            <Link href="/album-page">
+              <Image
+                src={song.albumCover} // Use the imported album picture
+                alt={`Album cover for ${song.songName}`}
+                width="100%" // Ensures the image takes the full width of the container
+              />
+            </Link>
           </AlbumCover>
 
           {/* Song name */}
           <SongName
-            onClick={() => navigate("/song-page")} // Navigate to SongPage when clicked
-            style={{ cursor: "pointer" }} // Change cursor to indicate it's clickable
+            style={{ cursor: 'pointer' }} // Change cursor to indicate it's clickable
           >
-            {song.songName}
+            <Link href="/song-page">{song.songName}</Link>
           </SongName>
 
           {/* Star rating */}
