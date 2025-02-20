2c96b71da8e9ce424c52ad07a626f362e5e02d64
Gayatri Puppala <gayatri.puppala03@gmail.com>
2025-02-20 00:33:10 -0800

    header doesn't display for LoginPage

--- jukeboxdapp/src/App.js
+++ jukeboxdapp/src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    useLocation,
+} from "react-router-dom";
 import FeedPage from "./pages/FeedPage";
 import PersonalProfilePage from "./pages/PersonalProfilePage";
 import ProfilePage from "./pages/ProfilePage";
@@ -10,20 +15,32 @@ import Header from "./bigcomponents/Header";
 import LoginPage from "./pages/LoginPage";
 import { AuthProvider } from "./firebase/auth";
 
+const AppContent = () => {
+    const location = useLocation();
+
+    return (
+        <>
+            {/* Conditionally render Header */}
+            {location.pathname !== "/" && <Header />}
+
+            <Routes>
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/feed" element={<FeedPage />} />
+                <Route path="/profile" element={<PersonalProfilePage />} />
+                <Route path="/album-page" element={<AlbumPage />} />
+                <Route path="/song-page" element={<SongPage />} />
+                <Route path="/artist-page" element={<ArtistPage />} />
+                <Route path="/profile-page" element={<ProfilePage />} />
+            </Routes>
+        </>
+    );
+};
+
 const App = () => {
     return (
         <AuthProvider>
             <Router>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<LoginPage />} />
-                    <Route path="/feed" element={<FeedPage />} />
-                    <Route path="/profile" element={<PersonalProfilePage />} />
-                    <Route path="/album-page" element={<AlbumPage />} />
-                    <Route path="/song-page" element={<SongPage />} />
-                    <Route path="/artist-page" element={<ArtistPage />} />
-                    <Route path="/profile-page" element={<ProfilePage />} />
-                </Routes>
+                <AppContent />
             </Router>
         </AuthProvider>
     );
