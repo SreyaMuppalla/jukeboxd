6230a92080e06469f086fbe7db6e187b1b0802ad
Anthony Chen <anchen082016@gmail.com>
2025-02-20 14:32:28 -0800

    Initialized Recoil for state management

--- jukeboxd-next/src/pages/_app.js
+++ jukeboxd-next/src/pages/_app.js
@@ -1,11 +1,12 @@
 import Header from '@/bigcomponents/Header';
 import '@/styles/globals.css';
+import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <RecoilRoot>
       <Header />
       <Component {...pageProps} />
-    </>
+    </RecoilRoot>
   );
 }
