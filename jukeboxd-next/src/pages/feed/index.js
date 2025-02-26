dbfcd5bf12ea9810fd39ef62b7a198f87d51355c
Gayatri Puppala <gayatri.puppala03@gmail.com>
2025-02-26 00:47:22 -0800

    redirect to login component for if user not logged in

--- jukeboxd-next/src/pages/feed/index.js
+++ jukeboxd-next/src/pages/feed/index.js
@@ -1,39 +1,46 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
-import { Background } from '../../styles/StyledComponents';
-import SongsCarousel from '../../bigcomponents/SongsCarousal';
-import Review from '../../bigcomponents/Review';
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import { Background } from "../../styles/StyledComponents";
+import SongsCarousel from "../../bigcomponents/SongsCarousal";
+import Review from "../../bigcomponents/Review";
+import ProtectedRoute from "@/smallcomponents/ProtectedRoute";
 
 const FeedPage = () => {
-  return (
-    <Background>
-      <SongsCarousel />
-      {/* Reviews from Friends */}
-      <Box
-        style={{
-          marginTop: '32px',
-          padding: '16px',
-          backgroundColor: '#333',
-          borderRadius: '16px',
-          width: '90%',
-          margin: '32px auto',
-        }}
-      >
-        {/* Reviews Section Header */}
-        <Typography
-          variant="h5"
-          style={{ color: '#fff', marginBottom: '16px', textAlign: 'center' }}
-        >
-          Reviews from Friends
-        </Typography>
+    return (
+        <ProtectedRoute>
+            <Background>
+                <SongsCarousel />
+                {/* Reviews from Friends */}
+                <Box
+                    style={{
+                        marginTop: "32px",
+                        padding: "16px",
+                        backgroundColor: "#333",
+                        borderRadius: "16px",
+                        width: "90%",
+                        margin: "32px auto",
+                    }}
+                >
+                    {/* Reviews Section Header */}
+                    <Typography
+                        variant="h5"
+                        style={{
+                            color: "#fff",
+                            marginBottom: "16px",
+                            textAlign: "center",
+                        }}
+                    >
+                        Reviews from Friends
+                    </Typography>
 
-        {/* Individual Reviews */}
-        <Review />
-        <Review />
-        <Review />
-      </Box>
-    </Background>
-  );
+                    {/* Individual Reviews */}
+                    <Review />
+                    <Review />
+                    <Review />
+                </Box>
+            </Background>
+        </ProtectedRoute>
+    );
 };
 
 export default FeedPage;
