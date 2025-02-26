90f7a8329ae159bcdc04d8a8088eb266ffd40192
Gayatri Puppala <gayatri.puppala03@gmail.com>
2025-02-26 00:54:21 -0800

    added ProtectedRoute component to all major pages

--- jukeboxd-next/src/pages/profile-page/index.js
+++ jukeboxd-next/src/pages/profile-page/index.js
@@ -15,116 +15,122 @@ import Review from "../../bigcomponents/Review";
 import pfp from "../../images/pfp.jpg"; // Add a placeholder profile pic
 import Image from "next/image";
 import { useRouter } from "next/router";
+import ProtectedRoute from "@/smallcomponents/ProtectedRoute";
 
 const ProfilePage = () => {
     return (
-        <Background>
-            <ProfileContainer>
-                {/* Profile Info Section */}
-                <ProfileInfo>
-                    {/* Profile Picture */}
-                    <ProfilePicContainer>
-                        <Image
-                            src={pfp}
-                            alt="Profile"
-                            style={{
-                                width: "150px",
-                                height: "150px",
-                                borderRadius: "50%",
-                                marginRight: "16px",
-                            }}
-                        />
-                    </ProfilePicContainer>
+        <ProtectedRoute>
+            <Background>
+                <ProfileContainer>
+                    {/* Profile Info Section */}
+                    <ProfileInfo>
+                        {/* Profile Picture */}
+                        <ProfilePicContainer>
+                            <Image
+                                src={pfp}
+                                alt="Profile"
+                                style={{
+                                    width: "150px",
+                                    height: "150px",
+                                    borderRadius: "50%",
+                                    marginRight: "16px",
+                                }}
+                            />
+                        </ProfilePicContainer>
 
-                    {/* Username and Stats */}
-                    <ProfileDetailsContainer>
-                        <ProfileDetails>
-                            <Typography
-                                variant="h4"
-                                style={{ color: "#fff", marginBottom: "8px" }}
-                            >
-                                Username
-                            </Typography>
-                        </ProfileDetails>
+                        {/* Username and Stats */}
+                        <ProfileDetailsContainer>
+                            <ProfileDetails>
+                                <Typography
+                                    variant="h4"
+                                    style={{
+                                        color: "#fff",
+                                        marginBottom: "8px",
+                                    }}
+                                >
+                                    Username
+                                </Typography>
+                            </ProfileDetails>
 
-                        {/* Stats aligned to the right */}
-                        <StatsContainer>
-                            <StatItem>
-                                <Typography
-                                    variant="h5"
-                                    style={{ color: "#1db954" }}
-                                >
-                                    ##
-                                </Typography>
-                                <Typography
-                                    variant="subtitle2"
-                                    style={{ color: "#b3b3b3" }}
-                                >
-                                    Reviews
-                                </Typography>
-                            </StatItem>
-                            <StatItem>
-                                <Typography
-                                    variant="h5"
-                                    style={{ color: "#1db954" }}
-                                >
-                                    ##
-                                </Typography>
-                                <Typography
-                                    variant="subtitle2"
-                                    style={{ color: "#b3b3b3" }}
-                                >
-                                    Upvotes
-                                </Typography>
-                            </StatItem>
-                            <StatItem>
-                                <Typography
-                                    variant="h5"
-                                    style={{ color: "#1db954" }}
-                                >
-                                    ##
-                                </Typography>
-                                <Typography
-                                    variant="subtitle2"
-                                    style={{ color: "#b3b3b3" }}
-                                >
-                                    Friends
-                                </Typography>
-                            </StatItem>
-                        </StatsContainer>
-                    </ProfileDetailsContainer>
-                </ProfileInfo>
+                            {/* Stats aligned to the right */}
+                            <StatsContainer>
+                                <StatItem>
+                                    <Typography
+                                        variant="h5"
+                                        style={{ color: "#1db954" }}
+                                    >
+                                        ##
+                                    </Typography>
+                                    <Typography
+                                        variant="subtitle2"
+                                        style={{ color: "#b3b3b3" }}
+                                    >
+                                        Reviews
+                                    </Typography>
+                                </StatItem>
+                                <StatItem>
+                                    <Typography
+                                        variant="h5"
+                                        style={{ color: "#1db954" }}
+                                    >
+                                        ##
+                                    </Typography>
+                                    <Typography
+                                        variant="subtitle2"
+                                        style={{ color: "#b3b3b3" }}
+                                    >
+                                        Upvotes
+                                    </Typography>
+                                </StatItem>
+                                <StatItem>
+                                    <Typography
+                                        variant="h5"
+                                        style={{ color: "#1db954" }}
+                                    >
+                                        ##
+                                    </Typography>
+                                    <Typography
+                                        variant="subtitle2"
+                                        style={{ color: "#b3b3b3" }}
+                                    >
+                                        Friends
+                                    </Typography>
+                                </StatItem>
+                            </StatsContainer>
+                        </ProfileDetailsContainer>
+                    </ProfileInfo>
 
-                {/* Reviews Section */}
-                <Box
-                    style={{
-                        marginTop: "32px",
-                        padding: "16px",
-                        backgroundColor: "#333",
-                        borderRadius: "16px",
-                        width: "90%",
-                        margin: "32px auto",
-                    }}
-                >
-                    {/* Reviews Section Header */}
-                    <Typography
-                        variant="h5"
+                    {/* Reviews Section */}
+                    <Box
                         style={{
-                            color: "#fff",
-                            marginBottom: "16px",
-                            textAlign: "center",
+                            marginTop: "32px",
+                            padding: "16px",
+                            backgroundColor: "#333",
+                            borderRadius: "16px",
+                            width: "90%",
+                            margin: "32px auto",
                         }}
                     >
-                        Recent Reviews
-                    </Typography>
+                        {/* Reviews Section Header */}
+                        <Typography
+                            variant="h5"
+                            style={{
+                                color: "#fff",
+                                marginBottom: "16px",
+                                textAlign: "center",
+                            }}
+                        >
+                            Recent Reviews
+                        </Typography>
 
-                    {/* Individual Reviews */}
-                    <Review />
-                    <Review />
-                    <Review />
-                </Box>
-            </ProfileContainer>
-        </Background>
+                        {/* Individual Reviews */}
+                        <Review />
+                        <Review />
+                        <Review />
+                    </Box>
+                </ProfileContainer>
+            </Background>
+        </ProtectedRoute>
     );
 };
 
--- jukeboxd-next/src/pages/profile/index.js
+++ jukeboxd-next/src/pages/profile/index.js
@@ -23,6 +23,7 @@ import pfp from "../../images/pfp.jpg"; // Add a placeholder profile pic
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useAuth } from "../../backend/auth.js";
+import ProtectedRoute from "@/smallcomponents/ProtectedRoute";
 
 const PersonalProfilePage = () => {
     const router = useRouter(); // Initialize navigation using Next.js router
@@ -45,131 +46,136 @@ const PersonalProfilePage = () => {
     };
 
     return (
-        <Background>
-            <ProfileContainer>
-                {/* Profile Info Section */}
-                <ProfileInfo>
-                    {/* Profile Picture */}
-                    <ProfilePicContainer>
-                        <Image
-                            src={pfp}
-                            alt="Profile"
-                            style={{
-                                width: "150px",
-                                height: "150px",
-                                borderRadius: "50%",
-                                marginRight: "16px",
-                            }}
-                        />
-                    </ProfilePicContainer>
+        <ProtectedRoute>
+            <Background>
+                <ProfileContainer>
+                    {/* Profile Info Section */}
+                    <ProfileInfo>
+                        {/* Profile Picture */}
+                        <ProfilePicContainer>
+                            <Image
+                                src={pfp}
+                                alt="Profile"
+                                style={{
+                                    width: "150px",
+                                    height: "150px",
+                                    borderRadius: "50%",
+                                    marginRight: "16px",
+                                }}
+                            />
+                        </ProfilePicContainer>
 
-                    {/* Username and Stats */}
-                    <ProfileDetailsContainer>
-                        <ProfileDetails>
-                            <Typography
-                                variant="h4"
-                                style={{ color: "#fff", marginBottom: "8px" }}
-                            >
-                                {user ? user.email : "My Username"}
-                            </Typography>
-                        </ProfileDetails>
+                        {/* Username and Stats */}
+                        <ProfileDetailsContainer>
+                            <ProfileDetails>
+                                <Typography
+                                    variant="h4"
+                                    style={{
+                                        color: "#fff",
+                                        marginBottom: "8px",
+                                    }}
+                                >
+                                    {user ? user.email : "My Username"}
+                                </Typography>
+                            </ProfileDetails>
 
-                        {/* Stats aligned to the right */}
-                        <StatsContainer>
-                            <StatItem>
-                                <Typography
-                                    variant="h5"
-                                    style={{ color: "#1db954" }}
-                                >
-                                    ##
-                                </Typography>
-                                <Typography
-                                    variant="subtitle2"
-                                    style={{ color: "#b3b3b3" }}
-                                >
-                                    Reviews
-                                </Typography>
-                            </StatItem>
-                            <StatItem>
-                                <Typography
-                                    variant="h5"
-                                    style={{ color: "#1db954" }}
-                                >
-                                    ##
-                                </Typography>
-                                <Typography
-                                    variant="subtitle2"
-                                    style={{ color: "#b3b3b3" }}
-                                >
-                                    Upvotes
-                                </Typography>
-                            </StatItem>
-                            <StatItem>
-                                <Typography
-                                    variant="h5"
-                                    style={{ color: "#1db954" }}
-                                >
-                                    ##
-                                </Typography>
-                                <Typography
-                                    variant="subtitle2"
-                                    style={{ color: "#b3b3b3" }}
-                                >
-                                    Friends
-                                </Typography>
-                            </StatItem>
-                        </StatsContainer>
-                    </ProfileDetailsContainer>
-                </ProfileInfo>
+                            {/* Stats aligned to the right */}
+                            <StatsContainer>
+                                <StatItem>
+                                    <Typography
+                                        variant="h5"
+                                        style={{ color: "#1db954" }}
+                                    >
+                                        ##
+                                    </Typography>
+                                    <Typography
+                                        variant="subtitle2"
+                                        style={{ color: "#b3b3b3" }}
+                                    >
+                                        Reviews
+                                    </Typography>
+                                </StatItem>
+                                <StatItem>
+                                    <Typography
+                                        variant="h5"
+                                        style={{ color: "#1db954" }}
+                                    >
+                                        ##
+                                    </Typography>
+                                    <Typography
+                                        variant="subtitle2"
+                                        style={{ color: "#b3b3b3" }}
+                                    >
+                                        Upvotes
+                                    </Typography>
+                                </StatItem>
+                                <StatItem>
+                                    <Typography
+                                        variant="h5"
+                                        style={{ color: "#1db954" }}
+                                    >
+                                        ##
+                                    </Typography>
+                                    <Typography
+                                        variant="subtitle2"
+                                        style={{ color: "#b3b3b3" }}
+                                    >
+                                        Friends
+                                    </Typography>
+                                </StatItem>
+                            </StatsContainer>
+                        </ProfileDetailsContainer>
+                    </ProfileInfo>
 
-                {/* Reviews Section */}
-                <Box
-                    style={{
-                        marginTop: "32px",
-                        padding: "16px",
-                        backgroundColor: "#333",
-                        borderRadius: "16px",
-                        width: "90%",
-                        margin: "32px auto",
-                    }}
-                >
-                    {/* Reviews Section Header */}
-                    <Typography
-                        variant="h5"
+                    {/* Reviews Section */}
+                    <Box
                         style={{
-                            color: "#fff",
-                            marginBottom: "16px",
-                            textAlign: "center",
+                            marginTop: "32px",
+                            padding: "16px",
+                            backgroundColor: "#333",
+                            borderRadius: "16px",
+                            width: "90%",
+                            margin: "32px auto",
                         }}
                     >
-                        Recent Reviews
-                    </Typography>
+                        {/* Reviews Section Header */}
+                        <Typography
+                            variant="h5"
+                            style={{
+                                color: "#fff",
+                                marginBottom: "16px",
+                                textAlign: "center",
+                            }}
+                        >
+                            Recent Reviews
+                        </Typography>
 
-                    {/* Individual Reviews */}
-                    <Review />
-                    <Review />
-                    <Review />
-                </Box>
-            </ProfileContainer>
-            {/* Logout Button */}
-            {user && !isLoggingOut && (
-                <Box
-                    sx={{
-                        position: "fixed",
-                        bottom: 16,
-                        right: 16,
-                    }}
-                >
-                    <Button
-                        variant="contained"
-                        color="secondary"
-                        onClick={handleLogout}
+                        {/* Individual Reviews */}
+                        <Review />
+                        <Review />
+                        <Review />
+                    </Box>
+                </ProfileContainer>
+                {/* Logout Button */}
+                {user && !isLoggingOut && (
+                    <Box
+                        sx={{
+                            position: "fixed",
+                            bottom: 16,
+                            right: 16,
+                        }}
                     >
-                        Log Out
-                    </Button>
-                </Box>
-            )}
-        </Background>
+                        <Button
+                            variant="contained"
+                            color="secondary"
+                            onClick={handleLogout}
+                        >
+                            Log Out
+                        </Button>
+                    </Box>
+                )}
+            </Background>
+        </ProtectedRoute>
     );
 };
 
