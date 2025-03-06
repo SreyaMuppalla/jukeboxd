2230f16a429ff4df5fa64774c3e6982f459597c2
SreyaMuppalla <sreyam2003@gmail.com>
2025-03-06 11:09:12 -0800

    Changed dotenv path

--- jukeboxd-next/src/pages/s3.js
+++ jukeboxd-next/src/pages/s3.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: 'jukeboxd-next/.env' });
+require('dotenv').config({ path: ".env" });
 const aws = require('aws-sdk');
 const { promisify } = require("util");
 const crypto = require("crypto");  
