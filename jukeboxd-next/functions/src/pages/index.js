691bfb1439eeb90bb8906bc8679b1e252cef6f3d
ritzz26 <ritagarwalrit@gmail.com>
2025-03-06 20:55:14 -0800

    fixed fs

--- jukeboxd-next/functions/src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-import LoginPage from "./login-page";
-import { AuthProvider } from "@/backend/auth";
-
-export default function Home() {
-    return <LoginPage />;
-}
