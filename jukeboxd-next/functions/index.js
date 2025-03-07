72fae380d6e9d3d1dfa46fb8a035874d2edcbf69
ritzz26 <ritagarwalrit@gmail.com>
2025-03-07 00:25:32 -0800

    init hosting files

--- jukeboxd-next/functions/index.js
+++ jukeboxd-next/functions/index.js
@@ -1,19 +1,50 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const admin = require('firebase-admin');
+const next = require('next');
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// Initialize admin app without config - it uses the default credentials
+admin.initializeApp();
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
+// Access Firebase services
+const auth = admin.auth();
+const db = admin.firestore();
+
+// Export Firebase auth and Firestore services for use in other files
+exports.auth = auth;
+exports.db = db;
+
+// // Simple test function to verify deployment works
+exports.helloWorld = onRequest((req, res) => {
+  logger.info("Hello logs!", {structuredData: true});
+  res.send("Hello from Firebase!");
+});
+
+// Next.js server function
+// We'll keep this commented until we confirm basic deployment works
+// exports.nextServer = onRequest({
+//   memory: "1GiB",  // Increase memory allocation for Next.js
+//   timeoutSeconds: 60  // Increase timeout
+// }, (req, res) => {
+//   const dev = false;  // Always set to false in production
+//   const dir = '.';    // Root directory for Next.js app
+  
+//   const app = next({ 
+//     dev, 
+//     conf: { 
+//       distDir: '.next',
+//       // Keep config minimal to avoid issues
+//     }
+//   });
+  
+//   const handle = app.getRequestHandler();
+  
+//   logger.info(`New request: ${req.url}`);
+  
+//   return app.prepare()
+//     .then(() => handle(req, res))
+//     .catch(error => {
+//       logger.error('Error during request handling:', error);
+//       res.status(500).send('Internal Server Error');
+//     });
 // });
--- jukeboxd-next/next.config.mjs
+++ jukeboxd-next/next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
   compiler: {
     styledComponents: true,
   },
-  output: 'export',
   images: {
     domains: ['i.scdn.co', 'lh3.googleusercontent.com'], // Allow loading images from spotify
-    unoptimized: true, // Required for static export
   },
+  distDir: 'build',
 };
 
 export default nextConfig;
