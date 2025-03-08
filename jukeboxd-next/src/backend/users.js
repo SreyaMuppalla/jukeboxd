d86df0be340299f12fa16d077ece76d09809e02f
Aditya Mehta <adityam2350@gmail.com>
2025-03-07 16:25:25 -0800

    Removed update username functionality

--- jukeboxd-next/src/backend/users.js
+++ jukeboxd-next/src/backend/users.js
@@ -97,36 +97,6 @@ export const updateUserBio = async (user_id, new_bio) => {
     }
 };
 
-export const updateUsername = async (user_id, new_username) => {
-    try {
-        if (!user_id || !new_username) {
-            throw new Error("Missing user_id or new_username parameter");
-        }
-
-        // Check if the new username already exists
-        const usernameInDB = await usernameExists(new_username);
-        if (usernameInDB) {
-            throw new Error(`Error: Username "${new_username}" already exists`);
-        }
-
-        const userRef = doc(db, "users", user_id);
-        const userDoc = await getDoc(userRef);
-
-        if (!userDoc.exists()) {
-            throw new Error("User not found");
-        }
-
-        // Proceed with updating the username if no conflict exists
-        await updateDoc(userRef, {
-            username: new_username
-        });
-
-        return { message: "Username updated successfully" };
-    } catch (error) {
-        throw error;
-    }
-};
-
 export const updateUserProfilePicture = async (user_id, profile_picture_url) => {
     try {
         if (!user_id || !profile_picture_url) {
